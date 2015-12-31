d88381858d08025222f239110af8fb15901f5c29
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:35:46 +1300

    Converted to PSR-2

--- code/extensions/GoogleShoppingFeedExtension.php
+++ code/extensions/GoogleShoppingFeedExtension.php
@@ -1,35 +1,38 @@
 <?php
 
-class GoogleShoppingFeedExtension extends DataExtension {
+class GoogleShoppingFeedExtension extends DataExtension
+{
     
-	/**
-	 * @var array
-	 */
-	private static $db = array(
+    /**
+     * @var array
+     */
+    private static $db = array(
         "RemoveFromShoppingFeed" => "Boolean"
-	);
+    );
     
     
     /**
-	 * @param FieldList
-	 */
-	public function updateSettingsFields(FieldList $fields) {
-		$tabset = $fields->findOrMakeTab('Root.Settings');
-		
-		$tabset->push(new HeaderField(_t(
+     * @param FieldList
+     */
+    public function updateSettingsFields(FieldList $fields)
+    {
+        $tabset = $fields->findOrMakeTab('Root.Settings');
+        
+        $tabset->push(new HeaderField(_t(
             'GoogleShoppingFeed.GoogleShoppingFeed',
             'Google Shopping Feed'
         )));
         
         $tabset->push(new CheckboxField("RemoveFromShoppingFeed"));
-	}
+    }
     
     
     /**
-	 * @param FieldList
-	 */
-	public function updateCMSFields(FieldList $fields) {
-        if(!method_exists($this->owner, "getSettingsFields")) {
+     * @param FieldList
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        if (!method_exists($this->owner, "getSettingsFields")) {
             $tabset = $fields->findOrMakeTab('Root.Settings');
             
             $tabset->push(new HeaderField(_t(
@@ -39,42 +42,47 @@ class GoogleShoppingFeedExtension extends DataExtension {
             
             $tabset->push(new CheckboxField("RemoveFromShoppingFeed"));
         }
-	}
+    }
     
     
     /**
      * Can we add this object to a shopping feed?
      * 
-	 * @return boolean
-	 */
-	public function canIncludeInGoogleShoppingFeed() {
-		$can = true;
+     * @return boolean
+     */
+    public function canIncludeInGoogleShoppingFeed()
+    {
+        $can = true;
 
         // If object does not link to the current website or absolute
         // link not set.
-		if($this->owner->hasMethod('AbsoluteLink')) {
-			$hostHttp = parse_url(Director::protocolAndHost(), PHP_URL_HOST);
-			$objHttp = parse_url($this->owner->AbsoluteLink(), PHP_URL_HOST);
+        if ($this->owner->hasMethod('AbsoluteLink')) {
+            $hostHttp = parse_url(Director::protocolAndHost(), PHP_URL_HOST);
+            $objHttp = parse_url($this->owner->AbsoluteLink(), PHP_URL_HOST);
 
-			if($objHttp != $hostHttp) {
-				$can = false;
-			}
-		} else
+            if ($objHttp != $hostHttp) {
+                $can = false;
+            }
+        } else {
             $can = false;
+        }
         
         // If no price or title.
-        if(!$this->owner->Title || !$this->owner->Price || !$this->owner->Condition || !$this->owner->Availability || !$this->owner->Brand || !$this->owner->MPN)
+        if (!$this->owner->Title || !$this->owner->Price || !$this->owner->Condition || !$this->owner->Availability || !$this->owner->Brand || !$this->owner->MPN) {
             $can = false;
-		
-        // Can any user view this item
-		if($can) $can = $this->owner->canView();
+        }
         
-        if($can && $this->owner->RemoveFromShoppingFeed)
+        // Can any user view this item
+        if ($can) {
+            $can = $this->owner->canView();
+        }
+        
+        if ($can && $this->owner->RemoveFromShoppingFeed) {
             $can = false;
+        }
 
-		$this->owner->invokeWithExtensions('alterCanIncludeInGoogleShoppingFeed', $can);
+        $this->owner->invokeWithExtensions('alterCanIncludeInGoogleShoppingFeed', $can);
 
-		return $can;
-	}
-    
+        return $can;
+    }
 }
